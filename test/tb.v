b3b30fc45a9a9c15d082c187ed177d892fda92ed
Anup50-maker <as4171@srmist.edu.in>
2025-09-09 11:05:14 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_stone_paper_scissors (
+  tt_um_stone_paper_scissors user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
